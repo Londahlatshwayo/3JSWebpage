96ac03d07f3f68d855e66f1c5779c65b5a46973d
LondaHlatshwayo <londahlatshwayo@gmail.com>
2026-02-23 20:28:01 +0200

    SceneRendering

--- main.js
+++ main.js
@@ -1,14 +1,73 @@
-import * as THREE from 'three';
-import * as THREE from 'three/webgpu';
-import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import * as THREE from "three";
+import {GLTFLoader} from 'three/addons/loaders/GLTFLoader.js';
 
-const width = window.innerWidth;
-const height = window.innerHeight;
-const scene = new THREE.Scene();
-scene.background = THREE.Color(0xff99);
-const camera = new THREE. PerspectiveCamera (50, width / height, 0.1, 100);
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(width, height);
+/////////////////////////////////////////////RENDERER/////////////////////////////////////////////
+const renderer = new THREE.WebGLRenderer({antialias: true});
+renderer.outputColorSpace = THREE.SRGBColorSpace;
+
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setClearColor(0x000000);
+renderer.setPixelRatio(window.devicePixelRatio)
 document.body.appendChild(renderer.domElement);
+
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+/////////////////////////////////////////////RENDERER/////////////////////////////////////////////
+
+
+
+/////////////////////////////////////////////CAMERA/////////////////////////////////////////////
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(35, window.innerWidth / window.innerHeight, 1, 1000);
+camera.position.set(0, 1, 10);
+/////////////////////////////////////////////CAMERA/////////////////////////////////////////////
+
+
+/////////////////////////////////////////////GROUNDPLANE/////////////////////////////////////////////
+const geometry = new THREE.PlaneGeometry(70, 100, 100, 100);
+geometry.rotateX(-Math.PI/2);
+const material = new THREE.MeshPhysicalMaterial({ color: 0x555555, side: THREE.DoubleSide});
+const groundMesh = new THREE.Mesh(geometry, material);
+groundMesh.castShadow = false;
+groundMesh.receiveShadow = true;
+scene.add(groundMesh);
+/////////////////////////////////////////////GROUNDPLANE/////////////////////////////////////////////
+
+
+/////////////////////////////////////////////BACKPLANE/////////////////////////////////////////////
+
+/////////////////////////////////////////////BACKPLANE/////////////////////////////////////////////
+
+
+/////////////////////////////////////////////LIGHTS/////////////////////////////////////////////
+const ambientLight = new THREE.AmbientLight(0xFFFFFF, .02);
+ambientLight.position.set(0, 0, 0);
+scene.add(ambientLight);
+
+const spotLight = new THREE.SpotLight(0xFFFFFF, 5000, 100, .1, .9);
+spotLight.position.set(0, 50, 0);
+spotLight.castShadow = true;
+spotLight.shadow.bias = -.0001;
+scene.add(spotLight);
+/////////////////////////////////////////////LIGHTS/////////////////////////////////////////////
+
+
+/////////////////////////////////////////////LOADMODEL/////////////////////////////////////////////
+const loader = new GLTFLoader().setPath('.vite/assets/models/');
+loader.load('SM_PortraitWeb-v1.glb', (gltf) => {
+    const portrait = gltf.scene;
+
+    portrait.traverse((child) => {
+        if (child.isMesh) {
+            child.geometry.center();
+            child.castShadow = true;
+            child.receiveShadow = true;
+        }
+    });
+
+    portrait.position.set (0, 0, 0);
+    scene.add(portrait)});
+/////////////////////////////////////////////LOADMODEL/////////////////////////////////////////////
+
 
 renderer.render(scene, camera);
