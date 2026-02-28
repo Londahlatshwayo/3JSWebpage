912ee94d0c7f1a5f11c3fa84ef6f59b1708dd9fa
LondaHlatshwayo <londahlatshwayo@gmail.com>
2026-02-28 17:26:43 +0200

    Creating materials, importing compressed texture and applying them

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 import * as THREE from "three";
 import {GLTFLoader} from 'three/addons/loaders/GLTFLoader.js';
+import getLayer from "./getLayer.js"
 import {OrbitControls} from 'three/addons/controls/OrbitControls.js';
+import { color, texture } from "three/tsl";
+import { TextureLoader } from "three/webgpu";
 
 /////////////////////////////////////////////RENDERER/////////////////////////////////////////////
 const renderer = new THREE.WebGLRenderer({antialias: true});
@@ -19,8 +22,8 @@ renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 
 /////////////////////////////////////////////CAMERA/////////////////////////////////////////////
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(35, window.innerWidth / window.innerHeight, 1, 1000);
-camera.position.set(0, 1, 10);
+const camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, .5, 1000);
+camera.position.set(0, 0, 1.8);
 /////////////////////////////////////////////CAMERA/////////////////////////////////////////////
 
 
@@ -38,27 +41,39 @@ controls.update();
 
 
 /////////////////////////////////////////////GROUNDPLANE/////////////////////////////////////////////
-const geometry = new THREE.PlaneGeometry(70, 100, 100, 100);
-geometry.rotateX(-Math.PI/2);
-const material = new THREE.MeshPhysicalMaterial({ color: 0x555555, side: THREE.DoubleSide});
-const groundMesh = new THREE.Mesh(geometry, material);
-groundMesh.castShadow = false;
-groundMesh.receiveShadow = true;
-scene.add(groundMesh);
+//const geometry = new THREE.PlaneGeometry(70, 100, 100, 100);
+//geometry.rotateX(-Math.PI/2);
+//const material = new THREE.MeshPhysicalMaterial({ color: 0x555555, side: THREE.DoubleSide});
+//const groundMesh = new THREE.Mesh(geometry, material);
+//groundMesh.castShadow = false;
+//groundMesh.receiveShadow = true;
+//scene.add(groundMesh);
 /////////////////////////////////////////////GROUNDPLANE/////////////////////////////////////////////
 
 
-/////////////////////////////////////////////BACKPLANE/////////////////////////////////////////////
-
-/////////////////////////////////////////////BACKPLANE/////////////////////////////////////////////
+/////////////////////////////////////////////BACKGROUND/////////////////////////////////////////////
+const gradientBackground = getLayer({
+  hue: 0.6,
+  numSprites: 8,
+  opacity: 0.25,
+  radius: 16,
+  size: 30,
+  z: -15.5,
+});
+scene.add(gradientBackground);
+/////////////////////////////////////////////BACKGROUND/////////////////////////////////////////////
 
 
 /////////////////////////////////////////////LIGHTS/////////////////////////////////////////////
-const ambientLight = new THREE.AmbientLight(0xFFFFFF, .01);
+const ambientLight = new THREE.AmbientLight(0xFFFFFF, .9);
 ambientLight.position.set(0, 0, 0);
 scene.add(ambientLight);
 
-const spotLight = new THREE.SpotLight(0xFFFFFF, 5000, 0, .1, 1);
+const hemispherLight = new THREE.HemisphereLight(0xFFFFFF, .9);
+hemispherLight.position.set(0, 0, 0);
+scene.add(hemispherLight);
+
+const spotLight = new THREE.SpotLight(0xFFFFFF, 5000, 0, .1, 0);
 spotLight.position.set(0, 50, 0);
 spotLight.castShadow = true;
 spotLight.shadow.bias = -.0001;
@@ -70,9 +85,19 @@ scene.add(spotLight);
 const loader = new GLTFLoader();
 loader.load('assets/models/SM_PortraitWeb-moved.glb', (gltf) => {
     const portrait = gltf.scene;
+    
+    const loader = new THREE.TextureLoader();
+    const texture = loader.load( 'assets/textures/T_Portrait_Albedo2048.webp' );
+    texture.colorSpace = THREE.SRGBColorSpace;
+    texture.flipY =false;
+    //texture.colorSpace = THREE.SRGBColorSpace;
+
+    const portraitMaterial = new THREE.MeshPhysicalMaterial({map:texture});
 
     portrait.traverse((child) => {
         if (child.isMesh) {
+            child.geometry.center();
+            child.material = portraitMaterial;
             child.castShadow = false;
             child.receiveShadow = true;
         }
